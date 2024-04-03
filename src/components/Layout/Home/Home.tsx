8240e9ea11a7d1cae1840a096e123d6bf2e5f2cd
L3-V1 <levimoraes100@gmail.com>
2024-04-03 20:33:01 -0300

    Criacao do componente 'ModalInfo'

--- src/components/Layout/Home/Home.tsx
+++ src/components/Layout/Home/Home.tsx
@@ -8,6 +8,7 @@ import { useData } from "@/contexts/DataContext"
 import { IconButton, Tab } from "@mui/material"
 import InfoIcon from '@mui/icons-material/Info'
 import ModalWrapper, { ModalAttributes } from "@/components/ModalWrapper"
+import ModalInfo from "./ModalInfo"
 
 const Home = () => {
     const { currentTab, setCurrentTab } = useData()
@@ -43,9 +44,7 @@ const Home = () => {
                 <Card.Footer>&copy; 2024</Card.Footer>
             </Card.Root>
 
-            <ModalWrapper ref={modalRef} title="Modal Title...">
-                <p className="text-justify">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vitae ex diam. Maecenas dictum feugiat accumsan. Phasellus sem sapien, porta eget blandit id, finibus non nisl. Proin volutpat massa vitae nibh maximus porta. Proin nec nulla ac leo sagittis sodales. Ut semper in eros sit amet auctor. Quisque rhoncus quam in libero egestas elementum. Duis non posuere erat. Nulla sed tempus tellus. Integer non ligula est. Proin vitae orci at lacus porttitor laoreet. Cras elementum rhoncus aliquam. Suspendisse quis rutrum enim.</p>
-            </ModalWrapper>
+            <ModalInfo ref={modalRef} title="Modal Title..." />
         </Container>
     )
 }
